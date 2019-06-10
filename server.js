3a5a586a2ba785ff7f5686106f6e04f2d0990ec3
emily baier <emilyrbaier@gmail.com>
2019-06-10 14:13:31 -0700

    eslint, travis, set up server and app

---
+++ server.js
@@ -0,0 +1,9 @@
+require('dotenv').config();
+const { app } = require('./lib/app');
+
+const PORT = process.env.PORT || 7891;
+
+app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log(`listening on ${PORT}...`);
+});
