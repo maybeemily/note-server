84db316ae6b8b7074e175b9669e2183106e0b07d
emily baier <emilyrbaier@gmail.com>
2019-06-11 11:03:36 -0700

    added 'get' route

--- test/notes.test.js
+++ test/notes.test.js
@@ -4,6 +4,13 @@ const mongoose = require('mongoose');
 const connect = require('../lib/utils/connect');
 const app = require('../lib/app');
 
+const createNote = note => {
+  return request(app)
+    .post('/api/v1/notes')
+    .send(note)
+    .then(res => res.body);
+};
+
 describe('notes routes', () => {
   beforeAll(() => {
     return connect();
@@ -28,6 +35,20 @@ describe('notes routes', () => {
           body: 'cool body',
           __v: 0
         });
+      });
+  });
+
+  it('can get a list of notes', async() => {
+    const notes = await Promise.all([
+      createNote({ title: 'cool', body: 'body2' }),
+      createNote({ title: 'cool', body: 'body3' }),
+      createNote({ title: 'cool', body: 'body' })
+    ]);
+
+    return request(app)
+      .get('/api/v1/notes')
+      .then(res => {
+        expect(res.body).toEqual(notes);
       });
   });
 
